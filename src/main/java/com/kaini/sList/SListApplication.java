005224209fa9aa89ccac03c62a2d23425369977c
kai3101 <kx.lok97@gmail.com>
2020-05-27 19:42:01 +0100

    first commit

---
+++ src/main/java/com/kaini/sList/SListApplication.java
@@ -0,0 +1,13 @@
+package com.kaini.sList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaini/sList/SListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaini.sList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
