58754409980106e07b8367c59483765a421d61a0
kai3101 <kx.lok97@gmail.com>
2020-06-02 00:28:41 +0100

    added userInfo files

--- src/main/java/com/kaini/sList/mapper/UserInfoMapper.java
+++ src/main/java/com/kaini/sList/mapper/UserInfoMapper.java
@@ -3,8 +3,11 @@ package com.kaini.sList.mapper;
 import com.kaini.sList.domain.UserInfo;
 import com.kaini.sList.domain.UserInfoExample;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface UserInfoMapper {
     long countByExample(UserInfoExample example);
 
